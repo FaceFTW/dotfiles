a234e2a3630270d84cfbd240f6a6aba9724fbfc7
Alex Westerman <alex@faceftw.dev>
2025-02-01 13:48:46 -0500

    Various Fixes (Squashed For clarity)
    Fix Linux Files
    
    Whoops
    
    Fix Templating in Linux part of pre_deploy
    
    Cleanup zshrc + remove Chezmoi configurations
    
    Fix deploy of bat
    
    Attempt to fix oh-my-posh install
    
    More Fixes
    
    Use -L flag for curl
    
    Fix tar
    
    Fix OMP theme in zshrc + more
    
    One last fix

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -11,12 +11,12 @@ winget install Microsoft.PowerShell
 :pwsh_exists
 pwsh -NoLogo -NoProfile -File .dotter/deploy_scripts/pull_deps.ps1
 {{/if}}
-{{$if (eq dotter.os "unix")}}
-{{$if (eq arch "x86_64")}}
+{{#if (eq dotter.os "unix")}}
+{{#if (eq arch "x86_64")}}
 chmod +x .dotter/deploy_scripts/deploy_linux.sh
 sh .dotter/deploy_scripts/deploy_linux.sh
 {{/if}}
-{{$if (eq arch "arm64")}}
+{{#if (eq arch "arm64")}}
 chmod +x .dotter/deploy_scripts/deploy_linux_arm.sh
 sh .dotter/deploy_scripts/deploy_linux_arm.sh
 {{/if}}
