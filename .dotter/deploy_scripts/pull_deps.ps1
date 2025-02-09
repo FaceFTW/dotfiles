213871869b23bda87c0ea83daf83fc0bdcdfbcd1
Alex Westerman <alex@faceftw.dev>
2025-02-09 16:25:45 -0500

    Update post-deploy hook on windows

--- .dotter/deploy_scripts/pull_deps.ps1
+++ .dotter/deploy_scripts/pull_deps.ps1
@@ -1,7 +1,7 @@
 # Versions for all the binaries we are pulling if they need it
 
 $oh_my_posh_version = "v24.19.0"
-$vim_w32_version = "9.1.0516"
+$vim_w32_version = "9.1.1065"
 $bat_version = "v0.25.0"
 
 # Remove Old stuff
@@ -11,7 +11,11 @@ Remove-Item -Path "~/.local/bin/vim.exe" -ErrorAction Ignore
 Remove-Item -Path "~/.local/bin/bat.exe" -ErrorAction Ignore
 Remove-Item -Path "~/.local/bin/_bat.ps1" -ErrorAction Ignore
 
-New-Item -ItemType Directory -Name tmp >$null
+If(Test-Path -LiteralPath "$Env:USERPROFILE/tmp"){
+	Remove-Item -LiteralPath "$Env:USERPROFILE/tmp" -Recurse -Force
+}
+
+New-Item -ItemType Directory -Name "tmp" -Path $Env:USERPROFILE  >$null
 
 # Oh-My-Posh
 Invoke-WebRequest `
@@ -21,18 +25,18 @@ Invoke-WebRequest `
 # Vim
 Invoke-WebRequest `
 	-Uri "https://github.com/vim/vim-win32-installer/releases/download/v$vim_w32_version/gvim_$($vim_w32_version)_x64.zip" `
-	-Outfile "tmp/vim.zip"
-Expand-Archive -Path "tmp/vim.zip" -DestinationPath tmp >$null
+	-Outfile "$Env:USERPROFILE/tmp/vim.zip"
+Expand-Archive -Path "$Env:USERPROFILE/tmp/vim.zip" -DestinationPath "$Env:USERPROFILE/tmp" >$null
 # NOTE This will change with vim version changes
-Move-Item -Path tmp/vim/vim91 -Destination ~/.local/bin/vim
-New-Item -ItemType SymbolicLink -Path "~/.local/bin/vim.exe" -Value "~/.local/bin/vim/vim.exe" >$null
+Move-Item -Path $Env:USERPROFILE/tmp/vim/vim91 -Destination ~/.local/bin/vim
+# New-Item -ItemType SymbolicLink -Path "~/.local/bin/vim.exe" -Value "~/.local/bin/vim/vim.exe" >$null
 
 # bat
 Invoke-WebRequest `
 	-Uri "https://github.com/sharkdp/bat/releases/download/$bat_version/bat-$bat_version-x86_64-pc-windows-msvc.zip" `
-	-OutFile "tmp/bat.zip"
-Expand-Archive -Path "tmp/bat.zip" -DestinationPath tmp
-Move-Item -Path "tmp/bat-$bat_version-x86_64-pc-windows-msvc/bat.exe" -Destination "~/.local/bin/bat.exe"
-Move-Item -Path "tmp/bat-$bat_version-x86_64-pc-windows-msvc/autocomplete/_bat.ps1" -Destination "~/.local/bin/_bat.ps1"
+	-OutFile "$Env:USERPROFILE/tmp/bat.zip"
+Expand-Archive -Path "$Env:USERPROFILE/tmp/bat.zip" -DestinationPath "$Env:USERPROFILE/tmp"
+Move-Item -Path "$Env:USERPROFILE/tmp/bat-$bat_version-x86_64-pc-windows-msvc/bat.exe" -Destination "~/.local/bin/bat.exe"
+Move-Item -Path "$Env:USERPROFILE/tmp/bat-$bat_version-x86_64-pc-windows-msvc/autocomplete/_bat.ps1" -Destination "~/.local/bin/_bat.ps1"
 
-Remove-Item -Path tmp -Recurse
+Remove-Item -Path $Env:USERPROFILE/tmp -Recurse
