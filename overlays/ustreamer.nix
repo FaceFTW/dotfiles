7a964ebd30ddd306b7838940544874b9b841a7ed
Alex Westerman <alex@faceftw.dev>
2025-09-23 20:53:40 -0400

    Automatically import defined overlays

--- overlays/ustreamer.nix
+++ overlays/ustreamer.nix
@@ -1,6 +1,6 @@
 # Webcam Daemon setup via ustreamer. Assumes Raspberry Pi environment
 final: prev: with prev; {
-  ustreamer = (
+  final.ustreamer = (
     pkgs.ustreamer.override {
       withSystemd = false;
       withJanus = false;
