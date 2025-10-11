efb9522f67157f56e3143d0b8c9ac621e822dbc0
Alex Westerman <alex@faceftw.dev>
2025-10-10 22:07:13 -0400

    Some finagling with moonraker/klipper

--- hosts/fabricator/klipper.nix
+++ hosts/fabricator/klipper.nix
@@ -1,11 +1,23 @@
 { ... }:
 {
+  users.users."klipper" = {
+    isSystemUser = true;
+    group = "klipper";
+    extraGroups = [
+       "spi"
+    ];
+  };
+  users.groups.klipper = { };
+
   services.klipper.enable = true;
+  services.klipper.user = "klipper";
+  services.klipper.group = "klipper";
   services.klipper.firmwares.fabricator.enable = true;
   services.klipper.firmwares.fabricator.configFile = ./klipper/Kconfig;
   services.klipper.configFile = ./klipper/printer.cfg;
 
   services.moonraker.enable = true;
+  services.moonraker.user = "klipper";
   services.moonraker.analysis.enable = true;
 
   services.mainsail.enable = true;
