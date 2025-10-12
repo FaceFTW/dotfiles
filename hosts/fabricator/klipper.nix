be064b5e03602e3934cfd256e0860fd8a5eff529
Alex Westerman <alex@faceftw.dev>
2025-10-11 23:02:34 -0400

    I think the klipper config is rectified

--- hosts/fabricator/klipper.nix
+++ hosts/fabricator/klipper.nix
@@ -1,22 +1,14 @@
 { ... }:
 {
-  users.users."klipper" = {
-    isSystemUser = true;
-    group = "klipper";
-    extraGroups = [
-       "spi"
-    ];
-  };
-  users.groups.klipper = { };
-
   services.klipper.enable = true;
-  services.klipper.user = "klipper";
-  services.klipper.group = "klipper";
   services.klipper.firmwares.fabricator.enable = true;
   services.klipper.firmwares.fabricator.configFile = ./klipper/Kconfig;
+  services.klipper.firmwares.fabricator.serial =
+    /dev/serial/by-id/usb-Klipper_stm32g0b0xx_36004C0001504E5238363120-if00;
   services.klipper.configFile = ./klipper/printer.cfg;
 
   services.moonraker.enable = true;
+  services.moonraker.group = "klipper";
   services.moonraker.user = "klipper";
   services.moonraker.analysis.enable = true;
 
