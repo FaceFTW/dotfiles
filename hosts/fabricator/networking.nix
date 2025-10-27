4b59b567b1233783ceaf505cc6e3b06b79655327
Alex Westerman <alex@faceftw.dev>
2025-10-26 20:46:37 -0400

    Separate out networking config because I might need to do some crazy things

---
+++ hosts/fabricator/networking.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  networking.hostName = "fabricator";
+  networking.firewall.allowedTCPPorts = [
+    22
+    80
+    5123
+    7125
+  ];
+  networking.wireless.enable = true;
+  networking.wireless.interfaces = [ "wlan0" ];
+  networking.wireless.secretsFile = config.sops.secrets.wifi_secrets.path;
+  networking.wireless.networks."Orbi89".pskRaw = "ext:home-psk";
+  networking.useDHCP = false;
+
+  systemd.network.enable = true;
+  systemd.network.networks."10-end0" = {
+    matchConfig.Name = "end0";
+    networkConfig.DHCP = "ipv4";
+    networkConfig.IPv6AcceptRA = true;
+    # linkConfig.RequiredForOnline = "routable";  - I Don't Think U
+  };
+  systemd.network.networks."10-wlan0" = {
+    matchConfig.Name = "wlan0";
+    networkConfig.DHCP = "ipv4";
+    networkConfig.IPv6AcceptRA = true;
+    linkConfig.RequiredForOnline = "routable";
+  };
+
+  services.avahi.enable = true;
+}
