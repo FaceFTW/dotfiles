177898beaf9b8773d3663a498ba6b756d8b98184
Alex Westerman <alex@faceftw.dev>
2025-12-11 23:10:28 -0500

    Initial nemesis provisioning

---
+++ hosts/nemesis/default.nix
@@ -0,0 +1,127 @@
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  user = "face";
+in
+{
+  imports = [
+    inputs.nixos-hardware.nixosModules.microsoft.surface.common
+    ../../modules/core.nix
+    ../../modules/kernel.nix
+    ../../modules/packages.nix
+    inputs.home-manager.nixosModules.home-manager
+    inputs.sops-nix.nixosModules.sops
+    inputs.disko.nixosModules.disko
+    ./disks.nix
+    ./networking.nix
+  ];
+
+  ############################################
+  # User Settings
+  ############################################
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.face = (import ./home-manager.nix);
+
+  users.users.${user} = {
+    home = "/home/${user}";
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+    ];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHn2LRPb2U5JR4lIKsZzXLofDvXeBinzC6a4s/+6G/5E awest@manifold"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKuQw4U+Wam1gjuEXyH/cObZfnfYiA/LPF0kjQPFTz9x face@manifold-wsl"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3fuhneqp6s6Ye9hHb60QrXq8vlu5INzeKlgiPtO5Pq alex@faceftw.dev"
+    ];
+    # initialPassword = ""; #For bootstrapping!
+    hashedPasswordFile = config.sops.secrets.user_passwd.path;
+    packages = [ ];
+  };
+
+  # The following are system users/groups defined for various services
+  # Unless they are defined elsewhere, in which here I document it for tracking
+  users.groups.wifi = { }; # For ensuring wpa_supplicant can access the secrets reasonably
+
+  ############################################
+  # SOPS Settings
+  ############################################
+  sops.defaultSopsFile = ./secrets.yaml;
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  sops.secrets.user_passwd.neededForUsers = true;
+  sops.secrets.wifi_secrets = {
+    group = config.users.users.systemd-network.group;
+  };
+
+  ############################################
+  # Nix Settings
+  ############################################
+  nix.nixPath = [
+    "nixos-config=/home/${user}/.config/dotfiles:/etc/nixos"
+    "nixpkgs=flake:nixpkgs"
+  ];
+  nix.settings.allowed-users = [ "${user}" ];
+  nix.settings.trusted-users = [
+    "@admin"
+    "${user}"
+    "@wheel"
+  ];
+  nix.package = pkgs.nix;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+    download-buffer-size = 1073741824
+  '';
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  ############################################
+  # Hardware Configuration
+  ############################################
+  hardware.microsoft-surface.kernelVersion = "stable";
+  hardware.microsoft-surface.ipts.enable = true;
+  hardware.microsoft-surface.surface-control.enable = true;
+
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.intelgpu.vaapiDrive="intel-media-driver";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.windows.
+
+  boot.kernelModules =
+    [ ];
+  boot.blacklistedKernelModules = [
+
+  ];
+
+  ############################################
+  # Program Options
+  ############################################
+  programs.zsh.enable = true;
+
+  ############################################
+  # Services
+  ############################################
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = false; # "Hardening"
+
+  ############################################
+  # Misc System Configuration
+  ############################################
+  time.timeZone = "America/New_York";
+
+  services.udev.enable = true;
+
+  ############################################
+  # Global Packages
+  ############################################
+  packages.monitoring = true;
+  packages.networking = true;
+  packages.secrets.base = true;
+  packages.
+
+  system.stateVersion = "25.05"; # Don't change this
+}
