92a82a33b078d50d0cd7cc81c45967972e6e727f
Alex Westerman <alex@faceftw.dev>
2025-12-13 23:36:41 -0500

    enable firmware update daemon

--- hosts/nemesis/services.nix
+++ hosts/nemesis/services.nix
@@ -18,6 +18,8 @@
   # If you want to use JACK applications, uncomment this
   #jack.enable = true;
 
+  services.fwupd.enable = true;
+
   # use the example session manager (no others are packaged yet so this is enabled by default,
   # no need to redefine it in your config for now)
   #media-session.enable = true;
