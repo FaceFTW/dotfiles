e785755bc05f1d1bc6321ef194b560c15472d253
Alex Westerman <alex@faceftw.dev>
2026-01-03 21:58:50 -0500

    Initial Nix Binary Cache on Archiver

--- hosts/archiver/services.nix
+++ hosts/archiver/services.nix
@@ -7,6 +7,93 @@
   };
   users.groups.watchman = { };
 
+  users.users.nix-serve = {
+    isSystemUser = true;
+    group = "nix-serve";
+    createHome = false;
+  };
+  users.groups.nix-serve = { };
+
+  systemd.services.nix-serve = {
+    description = "nix-serve binary cache server";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    path = [
+      pkgs.lix
+      pkgs.bzip2.bin
+    ];
+    environment.NIX_REMOTE = "daemon";
+
+    script = ''
+      export NIX_SECRET_KEY_FILE="$CREDENTIALS_DIRECTORY/NIX_SECRET_KEY_FILE"
+      exec ${pkgs.nix-serve}/bin/nix-serve --listen localhost:5000
+    '';
+
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = "5s";
+      User = "nix-serve";
+      Group = "nix-serve";
+      DynamicUser = true;
+      LoadCredential = "NIX_SECRET_KEY_FILE:/run/secrets/nix_cache_priv_key";
+      BindPath = "/mnt/motorway/nix/store:/nix/store";
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    config = ''
+      error_log stderr info;
+      events { }
+      http {
+          # Load mime types and configure maximum size of the types hash tables.
+          include ${pkgs.nginx}/conf/mime.types;
+          types_hash_max_size 2688;
+          default_type application/octet-stream;
+          ssl_protocols TLSv1.2 TLSv1.3;
+          ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305;
+          # $connection_upgrade is used for websocket proxying
+          map $http_upgrade $connection_upgrade {
+              default upgrade;
+              '''      close;
+          }
+          client_max_body_size 10m;
+          server_tokens off;
+
+          upstream nix-cache {
+              server localhost:5000 ;
+          }
+
+          server {
+              listen 0.0.0.0:80 ;
+              listen [::0]:80 ;
+              server_name archiver;
+              location ~ ^/(nix-cache)/ {
+                  rewrite ^/nix-cache/(.*) /$1 break;
+                  proxy_pass http://nix-cache;
+                  proxy_redirect off;
+                  proxy_http_version 1.1;
+                  proxy_set_header Upgrade $http_upgrade;
+                  proxy_set_header Connection $connection_upgrade;
+                  proxy_set_header Host $host;
+                  proxy_set_header X-Real-IP $remote_addr;
+                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              }
+          }
+      }
+    '';
+  };
+
+  services.logrotate.settings.nginx = {
+    files = [ "/var/log/nginx/*.log" ];
+    frequency = "weekly";
+    rotate = 26;
+    compress = true;
+    delaycompress = true;
+    postrotate = "[ ! -f /var/run/nginx/nginx.pid ] || kill -USR1 `cat /var/run/nginx/nginx.pid`";
+  };
+
   services.smartd.enable = true;
   services.smartd.defaults.monitored = "-a -m <nomailer> -M exec ${pkgs.smartd-notif-event}/bin/smartd-notif-event -s (S/../.././03|L/../(2|4)/./04)";
 
