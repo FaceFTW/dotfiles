2486ae45ebe3dadee810af9c7330c9e408311031
Alex Westerman <alex@faceftw.dev>
2026-02-01 23:44:42 -0500

    Ok but like actually fixed now

--- modules/packages/vscode.nix
+++ modules/packages/vscode.nix
@@ -8,33 +8,27 @@ let
   packages = config.packages;
   inherit (lib) mkIf mkMerge mkEnableOption;
 
-  # extensionsJson = builtins.fromJSON (
-  #   builtins.unsafeDiscardStringContext (builtins.readFile ../../config/vscode/extensions.json)
-  # );
-  extensionsNix =
-    builtins.map
-      (
-        extJson:
-        builtins.getAttr extJson.name (
-          builtins.getAttr extJson.author pkgs.nix-vscode-extensions.vscode-marketplace-universal
+  extenstionPatches = {
+    "rust-lang.rust-analyzer" =
+      {
+        pkgs,
+        lib,
+        system,
+      }:
+      {
+        buildInputs = [ pkgs.rust-analyzer ];
+      };
+  };
+
+  extensionsNix = pkgs.nix4vscode.forVscodeExt extenstionPatches (
+    builtins.map (ext: ext.identifier.id) (
+      builtins.filter (ext: ext.identifier ? uuid) (
+        builtins.fromJSON (
+          builtins.unsafeDiscardStringContext (builtins.readFile ../../config/vscode/extensions.json)
         )
       )
-      (
-        builtins.map
-          (arr: {
-            author = (builtins.elemAt (builtins.elemAt arr 1) 0);
-            name = (builtins.elemAt (builtins.elemAt arr 1) 1);
-          })
-          (
-            builtins.map (ext: builtins.split "([[:alnum:]|_-]+)\.([[:alnum:]|_-]+)" ext.identifier.id) (
-              builtins.filter (ext: ext.identifier ? uuid) (
-                builtins.fromJSON (
-                  builtins.unsafeDiscardStringContext (builtins.readFile ../../config/vscode/extensions.json)
-                )
-              )
-            )
-          )
-      );
+    )
+  );
 in
 {
   options.packages.vscode.enable = mkEnableOption "Visual Studio Code";
