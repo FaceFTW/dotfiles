b587da35570f94d7c015a341b8e6317a195e194d
Alex Westerman <alex@faceftw.dev>
2026-03-14 17:38:58 -0400

    fabricator fixes

--- hosts/fabricator/overlays/libcamera.nix
+++ hosts/fabricator/overlays/libcamera.nix
@@ -1,12 +1,12 @@
 # Using Raspberry Pi fork of libcamera
 final: prev: {
-  # libcamera = (
-  #   prev.libcamera.overrideAttrs {
-  #     src = prev.fetchgit {
-  #       url = "https://github.com/raspberrypi/libcamera.git";
-  #       rev = "f0e40f1c50bd0afe65727d6e407d0dcb42666ada";
-  #       hash = "sha256-sJKzmeeXD/66P5o+X9w3J2gwxDNsdBUdXEqU6goJdN4=";
-  #     };
-  #   }
-  # );
+  libcamera = (
+    prev.libcamera.overrideAttrs {
+      src = prev.fetchgit {
+        url = "https://github.com/raspberrypi/libcamera.git";
+        rev = "fe601eb6ffe02922ff980c60621dd79d401d9061";
+        hash = "sha256-pFvdy1sEGIVlIfIbBRdnNz7pVR4u5bMAL8UCtmUIVVs=";
+      };
+    }
+  );
 }
--- hosts/fabricator/services.nix
+++ hosts/fabricator/services.nix
@@ -16,8 +16,9 @@
     user = "klipper";
     group = "klipper";
     firmwares.fabricator.enable = true;
+    firmwares.fabricator.enableKlipperFlash = true;
     firmwares.fabricator.configFile = ./klipper/Kconfig;
-    firmwares.fabricator.serial = /dev/serial/by-id/usb-Klipper_stm32g0b0xx_36004C0001504E5238363120-if00;
+    firmwares.fabricator.serial = "/dev/serial/by-id/usb-Klipper_stm32g0b0xx_36004C0001504E5238363120-if00";
     configFile = ./klipper/printer.cfg;
   };
 
