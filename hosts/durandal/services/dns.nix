e2193418344cc0f01589df690570462cb49cc075
Alex Westerman <alex@faceftw.dev>
2026-05-18 21:55:27 -0400

    Fix DNS (i win)

--- hosts/durandal/services/dns.nix
+++ hosts/durandal/services/dns.nix
@@ -25,44 +25,46 @@
     listenOnPort = 1053;
     zones = [
       {
-        name = "faceftw.me";
-        file = "/etc/bind/zones/faceftw.me.zone";
+        name = "faceftw.local";
+        file = "/etc/bind/zones/faceftw.local.zone";
         master = true;
-        extraConfig = "allow-update { key rfc2136key.faceftw.me.; };";
+        extraConfig = "allow-update { key rfc2136key.faceftw.local.; };";
       }
     ];
   };
 
-  environment.etc."bind/zones/faceftw.me.zone" = {
+  environment.etc."bind/zones/faceftw.local.zone" = {
     enable = true;
     user = "named";
     group = "named";
     mode = "0644";
     text = ''
-      $ORIGIN faceftw.me.
-      $TTL 86400      ; 1 day
+      $ORIGIN faceftw.local.
+      $TTL    60   ; 86400 - 1 day
 
-      @                       IN SOA    dns root(
-                                                1337   ; Serial
-                                                3600   ; Refresh
-                                                300    ; Retry
-                                                3600   ; Expire
-                                                300)   ; Negative Cache TT
-                              IN NS     dns
+      @                   IN SOA  dns.faceftw.local. admin.faceftw.local. (
+                            1337   ; Serial
+                            3600   ; Refresh
+                            300    ; Retry
+                            3600   ; Expire
+                            300)   ; Negative Cache TT
+
+      @                   IN NS   dns.faceftw.local.
 
 
-      dns                     IN A      192.168.0.172
 
-      archiver                IN A      192.168.0.172
-      immich                  IN A      192.168.0.172
-      linkwarden              IN A      192.168.0.172
-      backrest                IN A      192.168.0.172
-      syncthing.archiver      IN A      192.168.0.172
-      garage                  IN A      192.168.0.172
-      jellyfin                IN A      192.168.0.172
+      dns                    IN A      192.168.0.172
 
-      durandal                IN A      192.168.0.7
-      pihole                  IN A      192.168.0.7
+      archiver               IN A      192.168.0.172
+      immich                 IN A      192.168.0.172
+      linkwarden             IN A      192.168.0.172
+      backrest               IN A      192.168.0.172
+      syncthing.archiver     IN A      192.168.0.172
+      garage                 IN A      192.168.0.172
+      jellyfin               IN A      192.168.0.172
+
+      durandal               IN A      192.168.0.7
+      pihole                 IN A      192.168.0.7
     '';
   };
 
