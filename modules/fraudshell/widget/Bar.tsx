a0753a890e69ce173c24ceb8df5386c9cd21fc5c
Alex Westerman <alex@faceftw.dev>
2026-06-14 19:38:42 -0400

    start with the workspaces part

--- modules/fraudshell/widget/Bar.tsx
+++ modules/fraudshell/widget/Bar.tsx
@@ -4,36 +4,29 @@ import { execAsync } from "ags/process"
 import { createPoll } from "ags/time"
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-  const time = createPoll("", 1000, "date")
-  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+	const time = createPoll("", 1000, 'date "+%m/%d/%Y %H:%M:%S"')
+	const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
-  return (
-    <window
-      visible
-      name="bar"
-      class="Bar"
-      gdkmonitor={gdkmonitor}
-      exclusivity={Astal.Exclusivity.EXCLUSIVE}
-      anchor={TOP | LEFT | RIGHT}
-      application={app}
-    >
-      <centerbox cssName="centerbox">
-        <button
-          $type="start"
-          onClicked={() => execAsync("echo hello").then(console.log)}
-          hexpand
-          halign={Gtk.Align.CENTER}
-        >
-          <label label="Welcome to AGS!" />
-        </button>
-        <box $type="center" />
-        <menubutton $type="end" hexpand halign={Gtk.Align.CENTER}>
-          <label label={time} />
-          <popover>
-            <Gtk.Calendar />
-          </popover>
-        </menubutton>
-      </centerbox>
-    </window>
-  )
+	return (
+		<window
+			visible
+			name="HUD"
+			class="FRAUDSHELL"
+			gdkmonitor={gdkmonitor}
+			exclusivity={Astal.Exclusivity.EXCLUSIVE}
+			anchor={TOP | LEFT | RIGHT}
+			application={app}
+		>
+			<Gtk.box
+			
+			<centerbox cssName="centerbox">
+				<menubutton $type="end" hexpand halign={Gtk.Align.CENTER}>
+					<label label={time} />
+					<popover>
+						<Gtk.Calendar />
+					</popover>
+				</menubutton>
+			</centerbox>
+		</window>
+	)
 }
