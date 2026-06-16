5e32ddc62036f8f0a2d8c41b47f0900a8a2ca5ae
Alex Westerman <alex@faceftw.dev>
2026-06-15 22:13:07 -0400

    Update some DNS settings on nemesis

--- hosts/nemesis/networking.nix
+++ hosts/nemesis/networking.nix
@@ -2,6 +2,11 @@
 {
   networking.hostName = "nemesis";
   networking.networkmanager.enable = true;
+  networking.nameservers = [ "192.168.0.7#dns.faceftw.local" ];
 
   services.resolved.enable = true;
+  services.resolved.settings.Resolve.FallbackDNS = [
+    "1.1.1.1#one.one.one.one"
+    "1.0.0.1#one.one.one.one"
+  ];
 }
