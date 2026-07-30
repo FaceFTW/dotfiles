73c6e2c67aa0cd7bbc2339a01db31fd090a64697
Alex Westerman <alex@faceftw.dev>
2026-07-29 20:52:14 -0400

    temp disable systemd resolved to debug dns issues on nemesis

--- hosts/nemesis/networking.nix
+++ hosts/nemesis/networking.nix
@@ -4,13 +4,13 @@
   networking.networkmanager.enable = true;
   networking.nameservers = [ "192.168.0.7#dns.faceftw.local" ];
 
-  services.resolved.enable = true;
-  services.resolved.settings.Resolve.DNS = [
-    "192.168.0.7"
-  ];
-  services.resolved.settings.Resolve.Domains = [ "~." ];
-  services.resolved.settings.Resolve.FallbackDNS = [
-    "1.1.1.1#one.one.one.one"
-    "1.0.0.1#one.one.one.one"
-  ];
+  # services.resolved.enable = true;
+  # services.resolved.settings.Resolve.DNS = [
+  #   "192.168.0.7"
+  # ];
+  # services.resolved.settings.Resolve.Domains = [ "~." ];
+  # services.resolved.settings.Resolve.FallbackDNS = [
+  #   "1.1.1.1#one.one.one.one"
+  #   "1.0.0.1#one.one.one.one"
+  # ];
 }
