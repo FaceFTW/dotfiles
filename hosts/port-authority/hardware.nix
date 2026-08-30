437a23739937a8401af582c7ec8b265c510a7840
Alex Westerman <alex@faceftw.dev>
2026-08-30 14:57:04 -0400

    Fix PoE overlay def.

--- hosts/port-authority/hardware.nix
+++ hosts/port-authority/hardware.nix
@@ -32,8 +32,23 @@
 
   hardware.raspberry-pi."4" = {
     apply-overlays-dtmerge.enable = true;
-    poe-plus-hat.enable = true;
+    # poe-plus-hat.enable = true;
   };
+
+  hardware.raspberry-pi.configtxt.deviceTreeOverlays."board-type=0x11" = [
+    {
+      rpi-poe-plus = {
+        poe_fan_temp0 = 40000;
+        poe_fan_temp0_hyst = 2000;
+        poe_fan_temp1 = 45000;
+        poe_fan_temp1_hyst = 2000;
+        poe_fan_temp2 = 50000;
+        poe_fan_temp2_hyst = 2000;
+        poe_fan_temp3 = 55000;
+        poe_fan_temp3_hyst = 5000;
+      };
+    }
+  ];
 
   hardware.deviceTree.enable = true;
   hardware.deviceTree.filter = "bcm2711-rpi-4*.dtb";
